56b0f7860bc2651b4523f8d54325aa540281b3ca
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-02-13 17:36:27 +0300

    lesson 2

---
+++ transaction/transactions_2.py
@@ -0,0 +1,19 @@
+from db_connection import MySQL,ExecuteError
+
+mysql = MySQL()
+
+mysql.execute("SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;")
+mysql.execute("START TRANSACTION;")
+
+try:
+    mysql.execute("SELECT @sum := 5000, @from := 4, @to := 2;")
+    mysql.execute("SELECT @balance_from := balance FROM accounts WHERE id = @from;")
+    mysql.execute("SELECT @balance_to := balance FROM accounts WHERE id = @to;")
+
+    mysql.sleep(5)
+
+    mysql.execute("UPDATE accounts SET balance = @balance_from - @sum WHERE id = @from;")
+    mysql.execute("UPDATE accounts SET balance = @balance_to + @sum WHERE id = @to;")
+    mysql.execute("COMMIT;")
+except ExecuteError:
+    mysql.execute("ROLLBACK;")
