c5dc9adeb07a60326e2279e74e41b562a57e768b
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-02-17 11:47:57 +0300

    add procedure

---
+++ routines/order_payment.py
@@ -0,0 +1,8 @@
+from transaction.db_connection import MySQL, ExecuteError
+
+mysql = MySQL(auto_commit=True)
+
+try:
+    mysql.execute("CALL order_payment(2, 7801, '2019-12-12 07:04:43')")
+except ExecuteError:
+    pass
