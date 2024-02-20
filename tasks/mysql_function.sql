4a44a504534116436866dc89a5e46cff6c2def8b
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-02-20 16:46:34 +0300

    functions

--- tasks/mysql_function.sql
+++ tasks/mysql_function.sql
@@ -1,6 +1,14 @@
+SET GLOBAL log_bin_trust_function_creators = 1;
+
 DELIMITER $$
 CREATE FUNCTION get_client_balance (client_id INT) RETURNS DECIMAL
 BEGIN
-    SELECT SUM(balance) FROM accounts WHERE user_id = client_id;
+	DECLARE client_balance DECIMAL DEFAULT 0;
+    SELECT SUM(balance) INTO client_balance FROM accounts WHERE user_id = client_id;
+	RETURN client_balance;
 END$$
 DELIMITER ;
+
+CALL get_client_balance(2);
+
+SELECT * FROM accounts WHERE balance > get_client_balance(2);
