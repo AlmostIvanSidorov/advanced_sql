5a1334947c8adbc7997b1b0e31660d407e415780
AlmostIvanSidorov <vanosidorov@gmail.com>
2024-02-21 17:00:14 +0300

    transaction procedure update

--- tasks/mysql_transaction_procedure.sql
+++ tasks/mysql_transaction_procedure.sql
@@ -1,10 +1,16 @@
 
 DROP PROCEDURE IF EXISTS order_payment;
+SET @from_balance := 50;
 DELIMITER $$
 CREATE PROCEDURE order_payment(order_id int, amount int , payment_date datetime )
 BEGIN
     DECLARE _rollback BOOL DEFAULT FALSE;
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET _rollback = TRUE;
+
+    SELECT balance INTO from_balance FROM accounts WHERE id = from_account_id;
+    IF from_balance < amount THEN
+        SET amount := from_balance;
+    AND IF;
 
     SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
     START TRANSACTION;
